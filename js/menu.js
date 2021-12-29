9cb756027bfd35a2259b470daf2134a4b44ee1ec
ariste <ariste.devstart@gmail.com>
2021-12-29 17:41:23 -0500

    Se corrige el nombre de la clase 'hamburguer' por 'hamburger'

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+// Seleccionamos las clases del HTML
+const hamburguer = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu-navegacion');
+
+console.log(menu);
+console.log(hamburger)
