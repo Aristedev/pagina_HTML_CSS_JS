ef98bc77f25d00e871ebd038a7ae2f5091f2e87e
ariste <ariste.devstart@gmail.com>
2022-01-18 12:06:59 -0500

    Se agrega funciones al archivo menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,17 @@
 // Seleccionamos las clases del HTML
-const hamburguer = document.querySelector('.hamburger');
+const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu-navegacion');
 
 console.log(menu);
-console.log(hamburger)
+console.log(hamburger);
+
+// Creamos un evento con 'hamburger'
+hamburger.addEventListener('click', () => {
+    // alert("clic")
+    menu.classList.toggle("spread") // menu desplegable
+});
+
+// Creamos un evneto cuando se haga 'click' la ventana
+window.addEventListener('click', e => {
+    console.log(e.target);
+})
